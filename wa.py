fb283b8b8160b491b9d161de36f4013bf9745c4b
Revence Kalibwani <revence@1st.ug>
2014-07-26 21:56:15 +0300

    Now handling and serving templates.

--- wa.py
+++ wa.py
@@ -1,10 +1,16 @@
 #!  /usr/bin/env python
 
 import cherrypy
+from lxml import html as templates
 import os, sys, thread
+from rapidsmsrw1000.apps.orm import orm
 import re
 import subprocess
 import web
+
+BMI_MIN = 19
+BMI_MAX = 25
+orm.ORM.connect(dbname  = 'thousanddays', user = 'thousanddays', host = 'localhost', password = 'thousanddays')
 
 class ThousandDays:
   def __init__(self, pth):
@@ -19,9 +25,39 @@ class ThousandCharts(ThousandDays):
   def charts(self):
     return ':-\\'
 
+  def process_template(self, pth, mapping, query):
+    ans = ''
+    with file(pth) as f:
+      doc = templates.fromstring(f.read())  # TODO: cache templates
+      for cle, val in mapping:
+        fin = doc
+        dem = cle
+        dat = val
+        if not hasattr(cle, '__iter__'):
+          dem = [cle]
+        for prc in dem:
+          if hasattr(prc, '__call__'):
+            fin = prc(fin)
+          else:
+            fin = fin.cssselect(prc)
+            if type(fin) == type([]):
+              try:
+                fin = fin[0]
+              except IndexError:
+                raise Exception, (u'No children in "%s" over %d.' % (prc, len(fin)))
+        if not (type(dat) in [type(x) for x in ['', u'', 0, 0L, 1.6182]]):
+          if type(dat) == type((object, 'method', ['args'])):
+            dat = getattr(dat[0], dat[1])(*dat[2:])
+          else:
+            raise Exception, ('How to process "%s" (%s) of "%s"?' % (dat, type(dat), cle))
+        for pc in (fin if type(fin) == type([]) else [fin]):
+          pc.text  = unicode(dat)
+      ans = templates.tostring(doc)
+    return ans
+
   @cherrypy.expose
-  def dynamised(self, chart = 'pregnancy', mapping = {}):
-    return file(os.path.join(self.path, 'html', '%s.html' % (chart, )))
+  def dynamised(self, chart = 'pregnancy', mapping = [], query = None):
+    return self.process_template(os.path.join(self.path, 'html', '%s.html' % (chart, )), mapping, query)
 
   @cherrypy.expose
   def delivery(self):
@@ -37,9 +73,117 @@ class ThousandCharts(ThousandDays):
 
   @cherrypy.expose
   def pregnancy(self):
-    return self.dynamised(mapping = {
-      '#layer_150 a:first':120
-    })
+    nat     = orm.ORM.query('pre_table',
+      cols      = ['COUNT(*) AS allpregs'],
+      annotate  = {
+        'coughing':('COUNT(*)', 'ch_bool IS NOT NULL'),
+        'diarrhoea':('COUNT(*)', 'di_bool IS NOT NULL'),
+        'fever':('COUNT(*)', 'fe_bool IS NOT NULL'),
+        'oedema':('COUNT(*)', 'oe_bool IS NOT NULL'),
+        'pneumo':('COUNT(*)', 'pc_bool IS NOT NULL'),
+        'disab':('COUNT(*)', 'db_bool IS NOT NULL'),
+        'cordi':('COUNT(*)', 'ci_bool IS NOT NULL'),
+        'necks':('COUNT(*)', 'ns_bool IS NOT NULL'),
+        'malaria':('COUNT(*)', 'ma_bool IS NOT NULL'),
+        'vomiting':('COUNT(*)', 'vo_bool IS NOT NULL'),
+        'stillb':('COUNT(*)', 'sb_bool IS NOT NULL'),
+        'jaun':('COUNT(*)', 'ja_bool IS NOT NULL'),
+        'hypoth':('COUNT(*)', 'hy_bool IS NOT NULL'),
+        'ibibari':('COUNT(*)', 'ib_bool IS NOT NULL')
+      },
+      migrations  = [
+        ('db_bool', False),
+        ('fe_bool', False),
+        ('ma_bool', False),
+        ('to_bool', False),
+        ('ch_bool', False),
+        ('vo_bool', False),
+        ('ja_bool', False),
+        ('ns_bool', False),
+        ('pc_bool', False),
+        ('ci_bool', False),
+        ('oe_bool', False),
+        ('di_bool', False),
+        ('sb_bool', False),
+        ('hy_bool', False),
+        ('hw_bool', False),
+        ('gs_bool', False),
+        ('mu_bool', False),
+        ('rm_bool', False),
+        ('ol_bool', False),
+        ('yg_bool', False),
+        ('kx_bool', False),
+        ('yj_bool', False),
+        ('lz_bool', False),
+        ('ib_bool', False)
+      ]
+    )
+    toi     = nat.specialise({'to_bool IS NOT NULL':''})
+    hnd     = nat.specialise({'hw_bool IS NOT NULL':''})
+    weighed = nat.specialise({'mother_height_float > 100.0 AND mother_weight_float > 15.0':''})
+    thins   = weighed.specialise({'(mother_weight_float / ((mother_height_float * mother_height_float) / 10000.0)) < %s': BMI_MIN})
+    fats    = weighed.specialise({'(mother_weight_float / ((mother_height_float * mother_height_float) / 10000.0)) > %s': BMI_MAX})
+    riskhsh = {'gs_bool IS NOT NULL OR mu_bool IS NOT NULL OR rm_bool IS NOT NULL OR ol_bool IS NOT NULL OR yg_bool IS NOT NULL OR kx_bool IS NOT NULL OR yj_bool IS NOT NULL OR lz_bool IS NOT NULL':''}
+    riskys  = nat.specialise(riskhsh)
+    rezes   = orm.ORM.query('res_table',
+      {},
+      cols        = ['COUNT(*) AS allreps'],
+    )
+    recovs  = rezes.specialise({'mw_bool IS NOT NULL':''})
+    aarecov = recovs.specialise({'aa_bool IS NOT NULL':''})
+    prrecov = recovs.specialise({'pr_bool IS NOT NULL':''})
+    total   = nat[0]['allpregs']
+    totalf  = float(total)
+    toils   = toi[0]['allpregs']
+    hands   = hnd[0]['allpregs']
+    toilpc  = 0.0
+    handpc  = 0.0
+    try:
+      toilpc  = (float(toils) / totalf) * 100.0
+      handpc  = (float(hands) / totalf) * 100.0
+    except ZeroDivisionError, zde:
+      pass
+    thinc   = thins[0]['allpregs']
+    fatc    = fats[0]['allpregs']
+    riskc   = riskys[0]['allpregs']
+    riskpc  = (float(riskc) / totalf) * 100.0
+    rez     = rezes[0]['allreps']
+    rezf    = float(rez)
+    rezpc   = (rezf / totalf) * 100.0
+    aac     = aarecov[0]['allreps']
+    aapc    = (float(aac) / rezf) * 100.0
+    prc     = prrecov[0]['allreps']
+    prpc    = (float(prc) / rezf) * 100.0
+    # TODO: do optimisations specialise?
+    return self.dynamised(mapping = [
+      ('#layer_150 a', nat[0]['allpregs']),
+      ('#risks69', round(prpc, 2)),
+      ('#risks66', round(aapc, 2)),
+      ('#risks65', aac),
+      ('#risks78', prc),
+      ('#layer_129', riskc),
+      ('#layer_89', round(riskpc, 2)),
+      ('#layer_89_0', rez),
+      ('#layer_75', round(rezpc, 2)),
+      ('#COUGHING24 strong', nat[0]['coughing']),
+      ('#DIARRHEA33 strong', nat[0]['diarrhoea']),
+      ('#FEVER12 strong', nat[0]['fever']),
+      ('#MALARIA09 strong', nat[0]['malaria']),
+      ('#VOMITING18 strong', nat[0]['vomiting']),
+      ('#STILLBORN21 strong', nat[0]['stillb']),
+      ('#EDEMA13 strong', nat[0]['oedema']),
+      ('#JAUNDICE09 strong', nat[0]['jaun']),
+      ('#PNEUMONIA21 strong', nat[0]['pneumo']),
+      ('#DISABILITY21 strong', nat[0]['disab']),
+      ('#HYPOTHEMIA21 strong', nat[0]['hypoth']),
+      ('#CLEFTPALATE21 strong', nat[0]['ibibari']),
+      ('#CORDINFECTION strong', nat[0]['cordi']),
+      ('#NECKSTIFFNESS strong', nat[0]['necks']),
+      ('#layer_12289 strong', hands),
+      ('#layer_10985 strong', toils),
+      ('#layer_21', fatc),
+      ('#layer_90', thinc),
+    ])
 
 class ChartMethods(cherrypy.dispatch.Dispatcher):
   def __call__(self, pth):
